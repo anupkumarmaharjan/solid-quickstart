d754ab3ab867b1c769f7288186cbb0b0484e1db5
Anup Kumar Maharjan <anupkumarmaharjan@gmail.com>
2023-04-26 22:18:52 +0545

    updated hompage

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,24 @@
 import { Title } from "solid-start";
-import Counter from "~/components/Counter";
+// import Counter from "~/components/Counter";
 
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
-      <Counter />
-      <p>
+      <Title>Palistha Saving & Credit Co-operative Ltd.</Title>
+      <h1 style={{"font-size": '48px'}}>Palistha Saving & Credit Co-operative Ltd.</h1>
+      <div style={{"font-size": '20px'}}>
+        <p>@ Thecho, Godawari Municipality-12, Lalitpur, Nepal</p>
+      <p>Call: 01-5572691</p>
+      <p>E-mail: <a href="mailto:palistha.sccl@gmail.com">palistha.sccl@gmail.com</a></p>
+      </div>
+      {/* <Counter /> */}
+      {/* <p>
         Visit{" "}
         <a href="https://start.solidjs.com" target="_blank">
           start.solidjs.com
         </a>{" "}
         to learn how to build SolidStart apps.
-      </p>
+      </p> */}
     </main>
   );
 }
